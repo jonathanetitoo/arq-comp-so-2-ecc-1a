aad0492966e46864d384cede4218c08a6b093655
Jonathan Tito <jotitoon@uide.edu.ec>
2026-04-02 01:58:25 +0000

    wip: my first script

---
+++ s5.sh
@@ -0,0 +1,12 @@
+# lista archivos y directorios de la carpeta actual en la que estoy
+ls
+# muestra el contenido de un archivo en la termina
+cat README.md
+# crea un directorio bajo el directorio en el que estoy trabajando
+mkdir padre1.txt
+# cambiamos al directorio padre1.txt
+cd padre1.txt
+# creamos un archivo
+touch hijo1.txt
+# me muevo al directorio padre del directorio actual
+cd ..
