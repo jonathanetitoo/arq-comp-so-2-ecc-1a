1b91dc1b444e62e90e183b255f3f55906c9b156e
Jonathan Tito <jotitoon@uide.edu.ec>
2026-04-15 04:14:17 +0000

    wip: ok

---
+++ powershell_script.ps1
@@ -0,0 +1,11 @@
+$a=2
+$b="chao"
+Write-Host $a
+Write-Output $b
+Write-Host "Hola" -ForegroundColor Green -BackgroundColor Yellow
+[int]$x = 10
+Write-Output $x
+[string]$y = "test"
+Write-Output $y
+[array]$z = @(1,2,3)
+Write-Host $z
