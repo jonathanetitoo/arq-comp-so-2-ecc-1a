018431430e98dc9541458ba93e5e2eb1aa7606bf
Jonathan Tito <jotitoon@uide.edu.ec>
2026-04-19 23:09:54 +0000

    wip: last class

--- powershell_script.ps1
+++ powershell_script.ps1
@@ -10,3 +10,24 @@ Write-Output $y
 [array]$z = @(1,2,3)
 Write-Host $z
 $c
+[int]$x = 2
+if ($x -gt 5) {
+ Write-Host "Mayor que 5"
+} else {
+ Write-Host "5 o menos"
+} 
+$lista = @(1, 2, 3)
+foreach ($i in $lista) {
+ Write-Host $i
+} 
+
+$x = 0
+while ($x -lt 3) {
+ Write-Host $x
+ $x++
+} 
+
+Function Sumar($a, $b) {
+ return $a + $b
+}
+Write-Host (Sumar 5 7) # Devuelve 12
